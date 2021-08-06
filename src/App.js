87663d66b8ad20fd1399c30e2cf677b89dad9c69
Daffa Arviano <daffa.arviano@outlook.com>
2021-08-06 20:31:24 +0700

    added new component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Success from 'Pages/Success';
 import AboutUs from 'Static/AboutUs';
 import Contact from 'Static/Contact';
 import VerificationNumber from 'Pages/VerificationNumber';
+import Under from 'Static/Under';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path="/cart" component={CartPages} />
           <Route path="/success" component={Success} />
           <Route path="/verification" component={VerificationNumber} />
+          <Route path="*" component={Under} />
         </Switch>
       </Router>
     </Provinder>
